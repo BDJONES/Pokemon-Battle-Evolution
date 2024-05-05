c84465ff2c1082c660354d6f32f79c7f8fcc57f2
Brandon Jones <72243416+BDJONES@users.noreply.github.com>
2024-05-05 10:48:36 -0400

    Update Pokemon.cs
    Changed some of the fields and added more fields in order to implement future changes.

--- Assets/Scripts/Classes/Pokemon.cs
+++ Assets/Scripts/Classes/Pokemon.cs
@@ -1,34 +1,38 @@
 #nullable enable
 
 using Cysharp.Threading.Tasks;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 
 public abstract class Pokemon: NetworkBehaviour
 {
     protected string speciesName = null!;
-    [SerializeField] protected string nickname = null!;
+    protected string nickname = null!;
     protected int level;
     protected List<Ability> abilityList= null!;
     [SerializeField] protected Ability ability = null!;
     [SerializeField] protected Gender gender;
     [SerializeField] protected Type Type1 = null!;
     protected Type? Type2;
-    [SerializeField] private StatusConditions status = StatusConditions.Healthy;
+    [SerializeField] private NetworkVariable<StatusConditions> status = new NetworkVariable<StatusConditions>(StatusConditions.Healthy);
     public StatusConditions Status { 
         get
         {
-            return status;
+            return status.Value;
         }
         set
         {
-            status = value;
+            status.Value = value;
             this.StatusEffect();
+            StatusChanged?.Invoke(status.Value);
         }
     }
+    public event Action<StatusConditions>? StatusChanged;
     [SerializeField] protected List<Attack> moveSet = null!;
     [SerializeField] protected List<Attack> learnSet = null!;
     protected bool isActive; // Is the pokemon currently on the field
@@ -48,118 +52,148 @@ public abstract class Pokemon: NetworkBehaviour
     [SerializeField] protected NetworkVariable<int> hpStat = new NetworkVariable<int>();
     protected int baseAttack;
     [SerializeField] protected int attackStat;
-    [SerializeField] private int attackStage = 0;
-    [SerializeField]
+    [SerializeField] private NetworkVariable<int> attackStage = new NetworkVariable<int>(0);
     public int AttackStage
     {
         get
         {
-            return attackStage;
+            return attackStage.Value;
         }
         set
         {
             
-            if (value > 6 || value < -6)
+            if (value < -6)
             {
-                Debug.Log("This stat can't be lowered any further");
+                GameManager.Instance.SendDialogueToClientRpc($"This stat can't be lowered any further");
+                GameManager.Instance.SendDialogueToHostRpc($"This stat can't be lowered any further");
                 return;
             }
-            attackStage = value;
+            if (value > 6)
+            {
+                GameManager.Instance.SendDialogueToClientRpc($"This stat can't be increased any further");
+                GameManager.Instance.SendDialogueToHostRpc($"This stat can't be increased any further");
+                return;
+            }
+            attackStage.Value = value;
             //Debug.Log($"Attack Stage = {attackStage}");
             //Debug.Log($"Base Attack = {baseAttack}");
             //Debug.Log($"Attack IVs = {ivs.attack}");
             //Debug.Log($"Attack Evs = {evs.attack}");
             //Debug.Log($"Level = {level}");
             attackStat = Mathf.FloorToInt(0.01f * (2 * baseAttack + ivs.attack + Mathf.FloorToInt(0.25f * evs.attack)) * level) + 5;
-            attackStat = Mathf.FloorToInt(attackStat * stageConversionDictionary[attackStage]);
+            attackStat = Mathf.FloorToInt(attackStat * stageConversionDictionary[attackStage.Value]);
         }
     }
     protected int baseDefense;
     [SerializeField] protected int defenseStat;
-    [SerializeField] private int defenseStage = 0;
+    [SerializeField] private NetworkVariable<int> defenseStage = new NetworkVariable<int>(0);
     public int DefenseStage
     {
         get
         {
-            return defenseStage;
+            return defenseStage.Value;
         }
         set
         {
 
-            if (value > 6 || value < -6)
+            if (value < -6)
             {
-                Debug.Log("This stat can't be lowered any further");
+                GameManager.Instance.SendDialogueToClientRpc($"This stat can't be lowered any further");
+                GameManager.Instance.SendDialogueToHostRpc($"This stat can't be lowered any further");
                 return;
             }
-            defenseStage = value;
+            if (value > 6)
+            {
+                GameManager.Instance.SendDialogueToClientRpc($"This stat can't be increased any further");
+                GameManager.Instance.SendDialogueToHostRpc($"This stat can't be increased any further");
+                return;
+            }
+            defenseStage.Value = value;
             defenseStat = Mathf.FloorToInt(0.01f * (2 * baseDefense + ivs.defense + Mathf.FloorToInt(0.25f * evs.defense)) * level) + 5;
-            defenseStat = Mathf.FloorToInt(defenseStat * stageConversionDictionary[defenseStage]);
+            defenseStat = Mathf.FloorToInt(defenseStat * stageConversionDictionary[defenseStage.Value]);
         }
     }
     protected int baseSpecialAttack;
     [SerializeField] protected int specialAttackStat;
-    [SerializeField] private int specialAttackStage = 0;
+    [SerializeField] private NetworkVariable<int> specialAttackStage = new NetworkVariable<int>(0);
     public int SpecialAttackStage
     {
         get
         {
-            return specialAttackStage;
+            return specialAttackStage.Value;
         }
         set
         {
 
-            if (value > 6 || value < -6)
-            {
-                Debug.Log("This stat can't be lowered any further");
+            if (value < -6) {
+                GameManager.Instance.SendDialogueToClientRpc($"This stat can't be lowered any further");
+                GameManager.Instance.SendDialogueToHostRpc($"This stat can't be lowered any further");
                 return;
             }
-            specialAttackStage = value;
+            if (value > 6)
+            {
+                GameManager.Instance.SendDialogueToClientRpc($"This stat can't be increased any further");
+                GameManager.Instance.SendDialogueToHostRpc($"This stat can't be increased any further");
+                return;
+            }
+            specialAttackStage.Value = value;
             specialAttackStat = Mathf.FloorToInt(0.01f * (2 * baseSpecialAttack + ivs.specialAttack + Mathf.FloorToInt(0.25f * evs.specialAttack)) * level) + 5;
-            specialAttackStat = Mathf.FloorToInt(specialAttackStat * stageConversionDictionary[specialAttackStage]);
+            specialAttackStat = Mathf.FloorToInt(specialAttackStat * stageConversionDictionary[specialAttackStage.Value]);
         }
     }
     protected int baseSpecialDefense;
     [SerializeField] protected int specialDefenseStat;
-    [SerializeField] private int specialDefenseStage = 0;
+    [SerializeField] private NetworkVariable<int> specialDefenseStage = new NetworkVariable<int>(0);
     public int SpecialDefenseStage
     {
         get
         {
-            return specialDefenseStage;
+            return specialDefenseStage.Value;
         }
         set
         {
 
-            if (value > 6 || value < -6)
+            if (value < -6)
             {
                 Debug.Log("This stat can't be lowered any further");
                 return;
             }
-            specialDefenseStage = value;
+            if (value > 6)
+            {
+                GameManager.Instance.SendDialogueToClientRpc($"This stat can't be increased any further");
+                GameManager.Instance.SendDialogueToHostRpc($"This stat can't be increased any further");
+                return;
+            }
+            specialDefenseStage.Value = value;
             specialDefenseStat = Mathf.FloorToInt(0.01f * (2 * baseSpecialDefense + ivs.specialDefense + Mathf.FloorToInt(0.25f * evs.specialDefense)) * level) + 5;
-            specialDefenseStat = Mathf.FloorToInt(specialDefenseStat * stageConversionDictionary[specialDefenseStage]);
+            specialDefenseStat = Mathf.FloorToInt(specialDefenseStat * stageConversionDictionary[specialDefenseStage.Value]);
         }
     }
     protected int baseSpeed;
     [SerializeField] protected int speedStat;
-    [SerializeField] private int speedStage = 0;
+    [SerializeField] private NetworkVariable<int> speedStage = new NetworkVariable<int>(0);
     public int SpeedStage
     {
         get
         {
-            return speedStage;
+            return speedStage.Value;
         }
         set
         {
-
-            if (value > 6 || value < -6)
+            if (value < -6)
             {
                 Debug.Log("This stat can't be lowered any further");
                 return;
             }
-            speedStage = value;
+            if (value > 6)
+            {
+                GameManager.Instance.SendDialogueToClientRpc($"This stat can't be increased any further");
+                GameManager.Instance.SendDialogueToHostRpc($"This stat can't be increased any further");
+                return;
+            }
+            speedStage.Value = value;
             speedStat = Mathf.FloorToInt(0.01f * (2 * baseSpeed + ivs.speed + Mathf.FloorToInt(0.25f * evs.speed)) * level) + 5;
-            speedStat = Mathf.FloorToInt(speedStat * stageConversionDictionary[speedStage]);
+            speedStat = Mathf.FloorToInt(speedStat * stageConversionDictionary[speedStage.Value]);
         }
     }
     [SerializeField] protected Ivs ivs = null!;
@@ -182,6 +216,16 @@ public abstract class Pokemon: NetworkBehaviour
         { -5, 0.29f },
         { -6, 0.25f }
     };
+    private NetworkVariable<bool> isPressured = new NetworkVariable<bool>(false);
+    public bool IsPressured
+    {
+        get 
+        { 
+            return isPressured.Value; 
+        } 
+    }
+    protected Attack? lastAttack = null;
+
     public string GetSpeciesName()
     {
         return speciesName;
@@ -216,31 +260,31 @@ public abstract class Pokemon: NetworkBehaviour
     }
     public bool IsHealthy()
     {
-        return this.status == StatusConditions.Healthy;
+        return this.status.Value == StatusConditions.Healthy;
     }
     public bool IsBurned()
     {
-        return this.status == StatusConditions.Burn;
+        return this.status.Value == StatusConditions.Burn;
     }
     public bool IsFrozen()
     {
-        return this.status == StatusConditions.Freeze;
+        return this.status.Value == StatusConditions.Freeze;
     }
     public bool IsParalyzed()
     {
-        return this.status == StatusConditions.Paralysis;
+        return this.status.Value == StatusConditions.Paralysis;
     }
     public bool IsPoisoned()
     {
-        return this.status == StatusConditions.Poison;
+        return this.status.Value == StatusConditions.Poison;
     }
     public bool IsBadlyPoisoned()
     {
-        return this.status == StatusConditions.BadPoison;
+        return this.status.Value == StatusConditions.BadPoison;
     }
     public bool IsAsleep()
     {
-        return this.status == StatusConditions.Asleep;
+        return this.status.Value == StatusConditions.Asleep;
     }
     public bool IsDead()
     {
@@ -315,6 +359,14 @@ public abstract class Pokemon: NetworkBehaviour
     {
         return speedStat;
     }
+    public Ivs GetIvs()
+    {
+        return ivs;
+    }
+    public Evs GetEvs()
+    {
+        return evs;
+    }
     public void RemoveItem()
     {
         heldItem = null;
@@ -325,7 +377,18 @@ public abstract class Pokemon: NetworkBehaviour
     }
     public void SetHPStat(int newHP)
     {
-        hpStat.Value = newHP;
+        if (newHP > GetMaxHPStat())
+        {
+            hpStat.Value = GetMaxHPStat();
+        }
+        else if (newHP < 0)
+        {
+            hpStat.Value = 0;
+        }
+        else
+        {
+            hpStat.Value = newHP;
+        }
     }
     public void SetAttackStat(int newAttack)
     {
@@ -347,20 +410,24 @@ public abstract class Pokemon: NetworkBehaviour
     {
         speedStat = newSpeed;
     }
+    public void SetPressuredStatus(bool status)
+    {
+        isPressured.Value = status;
+    }    
     public void StatusEffect()
     {
-        if (this.status == StatusConditions.Burn)
+        if (this.status.Value == StatusConditions.Burn)
         {
             // Fix Burn effect based on info here: https://the-episodes-and-movie-yveltal-and-more.fandom.com/wiki/Burn_(Pok%C3%A9mon_Status_Condition)#Generation_7
             // Damage Calculation already takes burn into effect
             return;
 
         }
-        else if (this.status == StatusConditions.Paralysis)
+        else if (this.status.Value == StatusConditions.Paralysis)
         {
             this.SetSpeedStat(Mathf.FloorToInt(0.01f * (2 * this.baseSpeed + this.ivs.speed + Mathf.FloorToInt(0.25f * this.evs.speed)) * level) + 5);
             this.SetSpeedStat(this.speedStat / 2);
-            this.SetSpeedStat(Mathf.FloorToInt(this.speedStat * stageConversionDictionary[speedStage]));
+            this.SetSpeedStat(Mathf.FloorToInt(this.speedStat * stageConversionDictionary[speedStage.Value]));
         }
     }
     public void ResetStatStages()
@@ -370,5 +437,17 @@ public abstract class Pokemon: NetworkBehaviour
         this.SpecialAttackStage = 0;
         this.SpecialDefenseStage = 0;
         this.SpeedStage = 0;
+    }
+    public void ResetBattleEffects()
+    {
+        isPressured.Value = false;
+    }
+    public void SetLastAttack(Attack attack)
+    {
+        this.lastAttack = attack;
+    }
+    public Attack? GetLastAttack()
+    {
+        return lastAttack;
     }
 }
