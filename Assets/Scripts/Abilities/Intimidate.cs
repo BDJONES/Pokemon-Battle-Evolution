ffdc440faca623facd5b14d88c06b577523aec35
Brandon Jones <72243416+BDJONES@users.noreply.github.com>
2024-05-06 00:42:27 -0400

    Update Intimidate.cs
    The Intimidate Ability now works effectively.

--- Assets/Scripts/Abilities/Intimidate.cs
+++ Assets/Scripts/Abilities/Intimidate.cs
@@ -1,18 +1,19 @@
+using Cysharp.Threading.Tasks;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 public class Intimidate : Ability
 {
-    //TrainerController trainerController;
     protected override void Awake()
     {
         //trainerController = this.abilityUser.transform.parent.gameObject.GetComponent<TrainerController>();
         base.Awake();
-        EventsToTriggerManager.OnTriggerEvent += EventToTriggerHandler;
+        eventsToTriggerManager.OnTriggerEvent += EventToTriggerHandler;
         this.abilityName = "Intimidate";
-        this.description = "When the Pokémon enters a battle, it intimidates opposing Pokémon and makes them cower, lowering their Attack stats.";
+        this.description = "When the Pokémon enters a battle, it intimidates opposing Pokémon and makes them cower, lowering their Attack stat.";
     }
 
     private void OnDestroy()
@@ -20,18 +21,37 @@ public class Intimidate : Ability
         GameManager.OnStateChange -= GameStateOnChangeHandler;
     }
 
-    protected override void TriggerEffect(Pokemon attacker, Pokemon target)
+    protected async override void TriggerEffect(Pokemon attacker, Pokemon target)
     {
-        Debug.Log("You intimidated the target");
-        target.AttackStage--;
+        int activeRPCs = GameManager.Instance.RPCManager.ActiveRPCs();
+        if (NetworkManager.Singleton.IsHost)
+        {
+            Debug.Log("The host is changing the attack value");
+            target.AttackStage--;
+        }
+        else
+        {
+            Debug.Log("The client is changing the attack value");
+            target.RequestStatChangeRpc(Stats.Attack, target.AttackStage - 1);
+        }
+        GameManager.Instance.SendDialogueToClientRpc($"{target.GetNickname()} was intimidated");
+        GameManager.Instance.SendDialogueToHostRpc($"{target.GetNickname()} was intimidated");
+        while (GameManager.Instance.RPCManager.ActiveRPCs() > activeRPCs)
+        {
+            await UniTask.Yield();
+        }
     }       
     
     private void EventToTriggerHandler(EventsToTrigger trigger)
     {
+
+        if (this.abilityUser == null) { return; }
         if (trigger == EventsToTrigger.YourPokemonSwitched)
         {
+            Debug.Log("Your Pokemon Switched");
             if (this.abilityUser.ActiveState == true)
             {
+                Debug.Log($"{abilityUser.GetSpeciesName()}, Intimidate is activating");
                 TriggerEffect(this.abilityUser, trainerController.GetOpponent().GetActivePokemon());
             }
         }
