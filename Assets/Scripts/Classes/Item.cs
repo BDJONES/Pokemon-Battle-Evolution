97b75c0e7eaaf96be6631d57abaf9148ccc10aee
Brandon Jones <72243416+BDJONES@users.noreply.github.com>
2024-05-14 22:05:16 -0400

    Update Item.cs
    Added new fields to the 'Item' class and a new function to go along with the held item effects and 'Effect Queue Controller'

--- Assets/Scripts/Classes/Item.cs
+++ Assets/Scripts/Classes/Item.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,13 +8,20 @@ public abstract class Item
     protected string itemName;
     protected string description;
     protected Pokemon holder;
+    protected int priority;
+    protected UIController uIController;
     public void SetHolder(Pokemon holder)
     {
         this.holder = holder;
     }
     protected abstract void InitializeItem();
-    public abstract void TriggerEffect(Pokemon holder);
-    public virtual void RevertEffect(Pokemon holder)
+
+    protected virtual void TriggerEffect(int userType)
+    {
+        return;
+    }
+
+    protected virtual void RevertEffect()
     {
         return;
     }
